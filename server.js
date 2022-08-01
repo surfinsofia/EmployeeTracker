dd5b6b0e567e9abf51854f90d8e9f8dcc1d14433
Sofia <asofiae96@gmail.com>
2022-08-01 14:43:54 -0500

    deleting extra files

--- server.js
+++ server.js
@@ -14,4 +14,12 @@ app.use((req, res) => {
     res.status(404).end();
   });
 
-  
+  // Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+      startInquirer();
+    });
+  });
